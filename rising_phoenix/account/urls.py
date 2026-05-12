b22842de582dc8365459b7d52a0c5398fa4775c8
Abdulmjeed Bojeir <AbdulmjeedJOB12@gmail.com>
2026-05-12 17:46:11 +0300

    profile view

--- rising_phoenix/account/urls.py
+++ rising_phoenix/account/urls.py
@@ -7,5 +7,7 @@ urlpatterns = [
     path('signup/', views.signup_view, name='signup_view'),
     path('login/', views.login_view, name="login_view"),
     path('logout/', views.logout_view, name='logout_view'),
-    path('artisan_signup/', views.artisan_signup_view, name='artisan_signup_view')
+    path('artisan_signup/', views.artisan_signup_view, name='artisan_signup_view'),
+    path('profile/<user_name>', views.profile_view, name='profile_view'),
+    path('profile/<user_name>/update', views.update_profile_view, name='update_profile_view')
 ]
