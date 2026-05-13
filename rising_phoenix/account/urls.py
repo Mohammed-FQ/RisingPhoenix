6d99b113933bf10a224f8377f92f594c052ac9fe
mansour134 <mansourmasood134@gmail.com>
2026-05-13 11:11:06 +0300

    verify adjustments

--- rising_phoenix/account/urls.py
+++ rising_phoenix/account/urls.py
@@ -9,7 +9,7 @@ urlpatterns = [
     path('logout/', views.logout_view, name='logout_view'),
     path('artisan_signup/', views.artisan_signup_view, name='artisan_signup_view'),
 
-    path('dashboard/', views.artisan_dashboard_view, name='artisan_dashboard_view'),`
+    path('dashboard/', views.artisan_dashboard_view, name='artisan_dashboard_view'),
     path('profile/<user_name>', views.profile_view, name='profile_view'),
     path('profile/<user_name>/update', views.update_profile_view, name='update_profile_view')
 ]
